5380e720ed6de1f326d718a285a50eba2d696885
YelenaDolgopolova <dolgopolova-lena.95@yandex.ru>
2021-08-06 23:34:00 +0500

    change files

---
+++ src/main/java/Manager/PostManager.java
@@ -0,0 +1,15 @@
+package Manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] search(long ownerId, String domain, String query, boolean ownersOnly,
+                         int count, int offSet) {
+        return null;
+    }
+    public void delete(int ownerId, int postId) {
+
+    }
+}
